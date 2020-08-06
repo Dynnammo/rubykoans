f63f6dadbca25b0f148ff9b3c2f894a9fce22917
dynnammo <baptiste.thivend@insa-lyon.fr>
2020-08-06 09:43:28 +0200

    Resolve about_inheritance

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+
+  def initialize(values = [])
+    @values = values
+  end
+
+  def roll(number_of_launch)
+    @values = []
+    number_of_launch.times { @values << ((rand(1..6) + Time.now.to_i) % 6 + 1) }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -48,6 +57,8 @@ class AboutDiceProject < Neo::Koan
     # If the rolls are random, then it is possible (although not
     # likely) that two consecutive rolls are equal.  What would be a
     # better way to test this?
+
+    # Include a time parameter that is different every second so that it decrease the chance to have the same numbers.
   end
 
   def test_you_can_roll_different_numbers_of_dice
